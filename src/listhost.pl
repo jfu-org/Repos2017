061108f34c93cacb0601f09e6c5366a76ab5d14c
猫山 <jfu000@gmail.com>
2018-09-28 19:09:19 +0900

    Add files via upload

---
+++ src/listhost.pl
@@ -0,0 +1,49 @@
+#!/bin/perl
+#
+# Script Name: listhost.pl 
+# Function   : create host_list.csv of ASI usecase from input file
+# Usage      : listhost.pl <input file>
+# Exit Code  : 0 ... Normal End
+#              1 ... Lack of columns
+#              2 ... No valid record
+#
+######################################################################
+#
+# History
+#
+# ID   Date       Name         Description             TaskID
+# ---- ---------- -----------  ----------------------- ------------
+# N/A  2018/03/16 Fujino        New Creation
+#
+######################################################################
+my $DEFLIST=$ARGV[0];
+open(INPUT, $DEFLIST) || die "Error: $! open($DEFLIST)";
+######################################################################
+my %host;
+my @list;
+my $num = 0;
+while(<INPUT>){
+	$_ =~ s/^\xEF\xBB\xBF//;   # remove BOM
+	next if /^\s*$/||/^\s*#/;
+	my @cols = split(/\s*,\s*/);
+	if( $#cols < 1 ){
+		print STDERR "Columns Insufficient: $_\n";
+		exit(1);
+	}
+	$cols[0] =~ s/^\s*//;  # remove blank
+	unless( $host{$cols[0]}){
+		$host{$cols[0]} = 1;
+		$list[$num] = $cols[0];
+		$num++;
+	} 
+}
+close(INPUT);
+unless( $num ){
+	print STDERR "No Valid Record Found\n";
+	exit(2);
+}
+######################################################################
+foreach my $node (@list){
+	print "$node, asiuser, /home/asitool/.ssh/id_rsa_ASI, AIX, 22\n";
+}
+######################################################################
